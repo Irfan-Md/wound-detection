150cd1748b40757459702db4ef82dce3cf413b55
IRFAN <70200836+Irfan-Md@users.noreply.github.com>
2022-02-04 13:25:40 +0530

    Add files via upload

---
+++ okok.py
@@ -0,0 +1,66 @@
+global net_input 
+net_input_saved = net_input.detach().clone()
+noise = net_input.detach().clone()
+out_avg = None
+last_net = None
+psrn_noisy_last = 0
+
+i = 0
+plot_counter =0
+def closure():
+    
+    global i, out_avg, psrn_noisy_last, last_net, net_input,plot_counter
+    
+    if reg_noise_std > 0:
+        net_input = net_input_saved + (noise.normal_() * reg_noise_std)
+    
+    out = net(net_input)
+    
+    # Smoothing
+    if out_avg is None:
+        out_avg = out.detach()
+    else:
+        out_avg = out_avg * exp_weight + out.detach() * (1 - exp_weight)
+            
+    total_loss = mse(out, img_noisy_torch)
+    total_loss.backward()
+        
+    
+    psrn_noisy = compare_psnr(img_noisy_np, out.detach().cpu().numpy()[0]) 
+    psrn_gt    = compare_psnr(img_np, out.detach().cpu().numpy()[0]) 
+    psrn_gt_sm = compare_psnr(img_np, out_avg.detach().cpu().numpy()[0]) 
+    
+    # Note that we do not have GT for the "snail" example
+    # So 'PSRN_gt', 'PSNR_gt_sm' make no sense
+    print ('Iteration %05d    Loss %f   PSNR_noisy: %f   PSRN_gt: %f PSNR_gt_sm: %f' % (i, total_loss.item(), psrn_noisy, psrn_gt, psrn_gt_sm), '\r', end='')
+    if  PLOT and i % show_every == 0:
+        out_np = torch_to_np(out)
+        plot_image_grid([np.clip(out_np, 0, 1), 
+                         np.clip(torch_to_np(out_avg), 0, 1)], factor=figsize, nrow=1)
+       
+        out_avg_to_export=np.array([np.clip(torch_to_np(out_avg), 0, 1)]).squeeze()*255
+        out_avg_to_export = np.transpose(out_avg_to_export, (1,2,0))
+        cv2.imwrite(os.getcwd()+'\\output\\'+'img_{}.png'.format(plot_counter), cv2.cvtColor(out_avg_to_export, cv2.COLOR_RGB2BGR))
+        writer.append_data(out_avg_to_export)
+        plot_counter+=1
+        
+    
+    # Backtracking
+    if i % show_every:
+        if psrn_noisy - psrn_noisy_last < -5: 
+            print('Falling back to previous checkpoint.')
+
+            for new_param, net_param in zip(last_net, net.parameters()):
+                net_param.data.copy_(new_param)
+
+            return total_loss*0
+        else:
+            last_net = [x.detach().cpu() for x in net.parameters()]
+            psrn_noisy_last = psrn_noisy
+            
+    i += 1
+
+    return total_loss
+
+p = get_params(OPT_OVER, net, net_input)
+optimize(OPTIMIZER, p, closure, LR, num_iter)
